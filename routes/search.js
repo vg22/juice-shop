6d2964216bac0b97013797a8b7063f4b00913372
Nate Michalov <nmichalov@gmail.com>
2021-11-16 09:56:28 -0500

    add inline ignore

--- routes/search.js
+++ routes/search.js
@@ -11,6 +11,7 @@ module.exports = function searchProducts () {
   return (req, res, next) => {
     let criteria = req.query.q === 'undefined' ? '' : req.query.q || ''
     criteria = (criteria.length <= 200) ? criteria : criteria.substring(0, 200)
+    // deepcode ignore Sqli: <please specify a reason of ignoring this>
     models.sequelize.query(`SELECT * FROM Products ORDER BY name`)
     // models.sequelize.query(`SELECT * FROM Products WHERE ((name LIKE '%${criteria}%' OR description LIKE '%${criteria}%') AND deletedAt IS NULL) ORDER BY name`)
       .then(([products]) => {
